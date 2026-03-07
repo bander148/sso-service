49f2a65631380cf842c00107df876cf91f7edd16
ban.der4 <ctepanskinder@yandex.ru>
2026-03-07 17:42:33 +0300

    update grpcapp

--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -1,7 +1,9 @@
-package grpc
+package grpcapp
 
 import (
+	"fmt"
 	"log/slog"
+	"net"
 	authgrpc "sso/internal/grpc/auth"
 
 	"google.golang.org/grpc"
@@ -13,6 +15,11 @@ type App struct {
 	port       int
 }
 
+func (a *App) MustRun() {
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
 func New(
 	log *slog.Logger,
 	port int,
@@ -25,4 +32,31 @@ func New(
 		gRPCServer: gRPCSserver,
 		port:       port,
 	}
+}
+func (a *App) Run() error {
+	const op = "grpcapp.Run"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.Int("port", a.port),
+	)
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		return fmt.Errorf("%s : %w", op, err)
+	}
+	log.Info("gRPC server is running", slog.String("addr", l.Addr().String()))
+
+	if err := a.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s : %w", op, err)
+	}
+	return nil
+}
+
+func (a *App) Stop() {
+	const op = "grpcapp.Stop"
+
+	a.log.With(slog.String("op", op)).Info("stopping gRPC server", slog.Int("port", a.port))
+
+	a.gRPCServer.GracefulStop()
 }
